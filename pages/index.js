97725495eb21508c9ce5c7c6d97110b170e7a7df
Anirudh <anirudh2403@gmail.com>
2020-07-16 18:52:52 +0530

    Adds style and defines css

--- pages/index.js
+++ pages/index.js
@@ -1,99 +1,33 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
+          <div className={utilStyles.container}>
       <Head>
-        <title>Create Next App</title>
+          <title >Anirudh Vyas</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Anirudh Vyas
+          <h1 className={utilStyles.heading2Xl}>
+          ANIRUDH V.
         </h1>
 
-        <p className="description">
+          <p className={utilStyles.headingXl}>
           Writing code and Sourcing Events at <a href='https://www.gojek.io/'>Gojek</a>
         </p>
       </main>
 
       <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: left;
-        }
 
         a {
           color: inherit;
           text-decoration: none;
         }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
       `}</style>
     </div>
-  )
+  );
 }
