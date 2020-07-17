8cdee8f8ae4b88cb8b4ce3606b7c8244d15ed528
Anirudh <anirudh2403@gmail.com>
2020-07-18 03:32:27 +0530

    Updates manifest and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,9 +42,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Anirudh's Blog`,
+        short_name: `Anirudh`,
+        description: `Anirudh Vyas, software developer. Writes about software and life.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
@@ -64,7 +64,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
     shouldAnalyseBundle && {
       resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
       options: {
