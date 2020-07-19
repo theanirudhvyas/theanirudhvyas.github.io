a32d04c6699f1b0ec015b91b05583b3885bbd200
Anirudh <anirudh2403@gmail.com>
2020-07-20 00:59:38 +0530

    Adds google analytics tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+          trackingId: 'UA-84227534-1',
       },
     },
     `gatsby-plugin-sitemap`,
